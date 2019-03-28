e979a79df34855b7ecb7c4fd758918a53a0c1f17
jsaavedra132 <48773058+jsaavedra132@users.noreply.github.com>
2019-03-27 19:41:47 -0500

    Mejoramiento
    Finalizacion bases de datos e hipervinculos en usuarios

--- VENV/main.py
+++ VENV/main.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask import render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import relationship, backref
 import os
 from werkzeug.security import generate_password_hash, check_password_hash
 
@@ -15,6 +16,7 @@ db = SQLAlchemy(app)
 user_logged = -1
 
 class Usuarios(db.Model):
+    __tablename__ = 'usuarios'
     id = db.Column(db.Integer, primary_key=True)
     usuario = db.Column(db.String(50), unique=True, nullable=False)
     nombre = db.Column(db.String(50), nullable=False)
@@ -23,31 +25,33 @@ class Usuarios(db.Model):
     celular = db.Column(db.Integer, nullable=False)
     correo = db.Column(db.String(50), nullable=False)
     universidad = db.Column(db.String(10), nullable=False)
+    addresses = db.relationship('Rutas', backref='usuarios', lazy="select")
 
 class Rutas(db.Model):
-    id_usuario = db.Column(db.String(50), db.ForeignKey("usuarios.id"), primary_key=True)
+    __tablename__='rutas'
+    id = db.Column(db.Integer, primary_key=True)
     inicio = db.Column(db.String(50), nullable=False)
     medio = db.Column(db.String(50), nullable=False)
-    finRuta = db.Column(db.String(50), nullable=False)
+    finruta = db.Column(db.String(50), nullable=False)
     hora = db.Column(db.String(50), nullable=False)
     fecha = db.Column(db.String(50), nullable=False)
+    usuario_id = db.Column(db.Integer, db.ForeignKey('usuarios.id'))
 
 @app.route("/")
 def index():
     return redirect(url_for('login'))
 
-if __name__ == "__main__":
-    db.create_all()
-    app.run()
-
 @app.route("/registro", methods=["GET", "POST"])
 def registro():
     if request.method == "POST":
-        hashed_password = generate_password_hash(request.form["contrasena"], method="sha256")
-        new_user = Usuarios(usuario=request.form["usuario"], nombre=request.form["nombre"], apellido=request.form["apellido"], contrasena=hashed_password, celular=request.form["celular"], correo=request.form["email"], universidad=request.form["uni"])
-        db.session.add(new_user)
-        db.session.commit()
-        return render_template('login.html')
+        try:
+            hashed_password = generate_password_hash(request.form["contrasena"], method="sha256")
+            new_user = Usuarios(usuario=request.form["usuario"], nombre=request.form["nombre"], apellido=request.form["apellido"], contrasena=hashed_password, celular=request.form["celular"], correo=request.form["email"], universidad=request.form["uni"])
+            db.session.add(new_user)
+            db.session.commit()
+            return render_template('login.html')
+        except Exception as e:
+            return render_template("error.html", e=e)
 
     return render_template('registro.html')
 
@@ -63,39 +67,55 @@ def tipo():
 def login():
     global user_logged
     if request.method == "POST":
+        try:
+            if '@' in request.form['usuario']:
+                user =  Usuarios.query.filter_by(correo=request.form["usuario"]).first()
+            else:
+                user = Usuarios.query.filter_by(usuario=request.form["usuario"]).first()
 
-        if '@' in request.form['usuario']:
-            user =  Usuarios.query.filter_by(correo=request.form["usuario"]).first()
-        else:
-            user = Usuarios.query.filter_by(usuario=request.form["usuario"]).first()
-
-        if user and check_password_hash(user.contrasena, request.form["contrasena"]): 
-            user_logged = user.id
-            return redirect(url_for('tipo'))
-
+            if user and check_password_hash(user.contrasena, request.form["contrasena"]): 
+                user_logged = user.id
+                return redirect(url_for('tipo'))
+        except:
+            return render_template("error.html")
+        
     return render_template('login.html')
 
 @app.route("/direccion", methods=["GET", "POST"])
 def direccion():
+    global user_logged
+    if request.method == "POST":
+        try:
+            new_ruta = Rutas(inicio=request.form["inicio"], medio=request.form["medio"], finruta=request.form["finruta"], hora=request.form["hora"], fecha=request.form["fecha"])
+            db.session.add(new_ruta)
+            db.session.commit()
+            usuario = Usuarios.query.filter_by(id = user_logged).first()
+            usuario.addresses.append(new_ruta)
+            db.session.commit()
+            return redirect(url_for('escogertipo'))
+        except Exception as e:
+            return render_template("error.html", e=e)
     if(user_logged<0):
         return redirect(url_for('login'))
     else:
         return render_template("direccion.html")
 
-    if request.method == "POST":
-        new_ruta = Rutas(inicio=request.form["inicio"], medio=request.form["medio"], finruta=request.form["finruta"], hora=request.form["hora"], fecha=request.form["fecha"])
-        db.session.add(new_ruta)
-        db.session.commit()
-        return redirect(url_for('direccion'))
-
-    
 
 @app.route("/usuario")
 def usuario():
+    rutas = []
+    rutaActual = []
     if(user_logged<0):
         return redirect(url_for('login'))
     else:
-        return render_template("usuario.html")
+        try:
+            for ruta in Rutas.query.all():
+                rutaActual = [Usuarios.query.filter_by(id=ruta.usuario_id).first(), ruta.inicio, ruta.medio, ruta.finruta, ruta.hora, ruta.fecha]
+                rutas.append(rutaActual)
+            return render_template("usuario.html", rutas=rutas)
+        except Exception as e:
+            return render_template("error.html", e=e)
+        
 
 @app.route("/escogertipo")
 def escogertipo():
@@ -103,3 +123,11 @@ def escogertipo():
         return redirect(url_for('login'))
     else:
         return render_template("escogertipo.html")
+
+
+if __name__ == "__main__":
+    try: 
+        db.create_all()
+        app.run()
+    except Exception as e:
+        print("Hubo un error en la aplicación: " + str(e))
